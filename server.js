10c88d24f39610100513fcb43497b98e4b2ae2b6
strelcov <ivan.strelcov.1986@gmail.com>
2016-08-25 18:15:21 +0300

    fix add user in server.js

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 var jsonServer = require('json-server');
 var server = jsonServer.create();
 var router = jsonServer.router({
-  "users": []
+  "users": [
+    {
+      "id": 1,
+      "image": "../../assets/img/a2.jpg",
+      "position": "Graphics designer",
+      "name": "John Smith",
+      "company_adress": "Riviera State 32/106",
+      "company": "Twitter, Inc.",
+      "phone": "(123) 456-7890",
+      "adress": {
+        "home_number": 795,
+        "street": "Folsom Ave",
+        "apartment": "Suite 600",
+        "city": "San Francisco",
+        "code": "CA 94107"
+      }
+    }
+  ]
 });
 var middlewares = jsonServer.defaults({
   static: __dirname + '/dist'
