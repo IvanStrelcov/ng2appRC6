d85896387794dae7087a68d5067d3ff78c6952c8
strelcov <ivan.strelcov.1986@gmail.com>
2016-08-30 17:20:13 +0300

    fix header template, add pg in dependencies

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 var jsonServer = require('json-server');
 var server = jsonServer.create();
 var router = jsonServer.router('.db.json');
+
+var pg = require('pg');
+var DATABASE_URL = 'postgres://ljkysafegvycdv:t9oRGvf2DzC2VT9Lew0C6F23PZ@ec2-54-247-127-6.eu-west-1.compute.amazonaws.com:5432/d3o0rqr9kbtajh';
+pg.defaults.ssl = true;
+pg.connect(DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
+
 var middlewares = jsonServer.defaults({
   static: __dirname + '/dist'
 });
